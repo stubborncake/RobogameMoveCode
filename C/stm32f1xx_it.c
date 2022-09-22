34aff8533fbf24354b0b7d18dc1829025df931b0
stubborncake <80664495+stubborncake@users.noreply.github.com>
2022-09-22 20:34:49 +0800

    Add files via upload

--- C/stm32f1xx_it.c
+++ C/stm32f1xx_it.c
@@ -256,91 +256,14 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {	
 	if(htim==&htim6) // 判断是哪一个定时器
 	{
-		static int time;
+		static int time;//控制间隔多长时间进入中断函数的time变量
 		time++;		
-		static int pwm1=0,pwm2=0,pwm3=0,pwm4=0;
-		static float enc1=0,enc2=0,enc3=0,enc4=0;
-		static float testnumber[2];
-		static int enc;
   if(htim->Instance == htim6.Instance)
   {
 		//每50ms进入一次PID计算中断
 		if(time % 50 == 0)
 		{		
-			//单环速度PID
-			if(DoubleBegin==0)
-			{	
-			enc1=Read_Encoder(1);
-			if(enc1<0) enc1=-enc1;
-			pwm1=PID_calc(&motor1_speed,enc1,target1);
-			if(pwm1<0) pwm1=0;
-			__HAL_TIM_SetCompare(&htim4,TIM_CHANNEL_1,pwm1);		
-									
-			enc2=Read_Encoder(2);
-			if(enc2<0) enc2=-enc2;
-			pwm2=PID_calc(&motor2_speed,enc2,target2);
-			if(pwm2<0) pwm2=0;
-			__HAL_TIM_SetCompare(&htim4,TIM_CHANNEL_2,pwm2);	
-						
-			enc3=Read_Encoder(3);
-			if(enc3<0) enc3=-enc3;
-			pwm3=PID_calc(&motor3_speed,enc3,target3);
-			if(pwm3<0) pwm3=0;
-			__HAL_TIM_SetCompare(&htim4,TIM_CHANNEL_3,pwm3);	
-			
-			enc4=Read_Encoder(8);
-			if(enc4<0) enc4=-enc4;
-			pwm4=PID_calc(&motor4_speed,enc4,target4);
-			if(pwm4<0) pwm4=0;
-			__HAL_TIM_SetCompare(&htim4,TIM_CHANNEL_4,pwm4);	
-			
-			//这里是PID曲线调参
-			static uint8_t tempData[12] = {0,0,0,0,0,0,0,0,0,0,0x80,0x7F};
-			testnumber[0]=enc4;
-			testnumber[1]=100.0;
-			memcpy(tempData, (uint8_t *)&testnumber, sizeof(testnumber));
-			HAL_UART_Transmit(&huart3,(uint8_t *)tempData,12,0x000A);
-			}
-			//双环位置PID
-			else if(DoubleBegin==1)
-			{	
-			if(Again==1)
-			{
-				enc1=enc2=enc3=enc4=0;
-				Again=0;
-			}			
-			enc=Read_Encoder(1);
-			if(enc<0) enc=-enc;
-      enc1 = enc1 + enc;
-			pwm1 = PID_calc(&motor1_position,enc1,target1);//位置环
-			pwm1 = PID_calc(&motor1_speed,enc,pwm1);//速度环	
-			if(pwm1<0) pwm1=0;
-			__HAL_TIM_SetCompare(&htim4,TIM_CHANNEL_1,pwm1);
-
-			enc=Read_Encoder(2);
-			if(enc<0) enc=-enc;
-			enc2 = enc2 + enc;
-			pwm2 = PID_calc(&motor2_position,enc2,target2);//位置环
-			pwm2 = PID_calc(&motor2_speed,Read_Encoder(2),pwm2);//速度环	
-			if(pwm2<0) pwm2=0;
-			__HAL_TIM_SetCompare(&htim4,TIM_CHANNEL_2,pwm2);
-			
-			enc=Read_Encoder(3);
-			if(enc<0) enc=-enc;
-			enc3 = enc3 + enc;
-			pwm3 = PID_calc(&motor3_position,enc3,target3);//位置环
-			pwm3 = PID_calc(&motor3_speed,enc,pwm3);//速度环	
-			if(pwm3<0) pwm3=0;
-			__HAL_TIM_SetCompare(&htim4,TIM_CHANNEL_3,pwm3);
-			
-			enc=Read_Encoder(8);
-			if(enc<0) enc=-enc;
-			enc4 = enc4 + enc;
-			pwm4 = PID_calc(&motor4_position,enc4,target4);//位置环
-			pwm4 = PID_calc(&motor4_speed,Read_Encoder(8),pwm4);//速度环	
-			if(pwm4<0) pwm4=0;
-			__HAL_TIM_SetCompare(&htim4,TIM_CHANNEL_4,pwm4);			
-			}			
+			PidSet();
 		}		
 		if(time >= 1000) time = 0;							
   }
